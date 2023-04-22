f0c8eef59c8fe98945b646e30b5cfa60cdef1206
QuangTrung <quangtrungtv712@gmail.com>
2023-04-22 21:24:50 +0700

    leeson 05 update

--- src/lesson_05/StringLearning.java
+++ src/lesson_05/StringLearning.java
@@ -1,5 +1,7 @@
 package lesson_05;
 
+import java.util.Arrays;
+
 public class StringLearning {
     public static void main(String[] args) {
         String myName1 = "Teo"; //Literal declaration
@@ -22,8 +24,8 @@ public class StringLearning {
         int totalUpperCases = 0;
 
         for (char myCharacter : myCharacters) {
-            if(Character.isDigit(myCharacter)) totalDigits++;
-            else if(Character.isUpperCase(myCharacter)) totalUpperCases++;
+            if (Character.isDigit(myCharacter)) totalDigits++;
+            else if (Character.isUpperCase(myCharacter)) totalUpperCases++;
             else if (Character.isLowerCase(myCharacter)) totalLowerCases++;
 
         }
@@ -32,7 +34,7 @@ public class StringLearning {
         System.out.println("totalUpperCases = " + totalUpperCases);
         System.out.println("totalLowerCases = " + totalLowerCases);
 
-        if(totalDigits > 0 && totalLowerCases > 0 && totalUpperCases > 0){
+        if (totalDigits > 0 && totalLowerCases > 0 && totalUpperCases > 0) {
             System.out.println("Your Password Is Good");
         } else {
             System.out.println("Invailid Password");
@@ -44,6 +46,48 @@ public class StringLearning {
         System.out.println(myBadWord);
         System.out.println(filteredStr);
 
+        // Trim het khoang trang
+        System.out.println(myBadWord.trim());
 
+        // Subtring, indexOf, split
+        String url = "http://google.com";
+        System.out.println(url.length());
+        System.out.println(url.indexOf("g"));
+        System.out.println(url.indexOf("http:"));
+        System.out.println(url.indexOf("http:_"));
+
+        System.out.println(url.substring(0, 5));
+        System.out.println(url.substring(2, 5));
+        System.out.println(url.substring(2, url.length()));
+        System.out.println(url.substring(2));
+
+        // split
+        String[] splitStr = url.split("");
+        System.out.println(Arrays.toString(splitStr));
+
+        String[] splitStr2 = url.split("://");
+        System.out.println(Arrays.toString(splitStr2));
+
+        // Regex | Regular Expression
+        String myCookingTime = "    105  min  ";
+        // TODO: Pattern and Matcher // nen doc them
+
+        String cookingTimeNumStr = myCookingTime.replaceAll("[^0-9]", "");
+        System.out.println(cookingTimeNumStr); // nay la string chu so
+        System.out.println(Integer.valueOf(cookingTimeNumStr)); // nay la con so
+
+        int cookingTime = Integer.valueOf(cookingTimeNumStr);
+        System.out.println("Cooking Time: " + cookingTime + 1);
+
+        System.out.println("Cooking Time: " + (cookingTime + 1)); // (cookingTime + 1) bao tron thi moi tinh toan, khong se la noi chuoi
+
+
+        // Concatenation
+        String s1 = "Hello, ";
+        String s2 = "Teo";
+        System.out.println(s1.concat(s2));
+        System.out.println(s1 + s2);
+
+        // String Builder doc them (  noi chuoi lon )
     }
 }
